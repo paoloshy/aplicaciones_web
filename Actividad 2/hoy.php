46f8ee069da996c769b0a37a045fccc6aa4fcdc5
paoloshy <114453757+paoloshy@users.noreply.github.com>
2022-10-26 00:02:40 -0500

    Add files via upload

---
+++ Actividad 2/hoy.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>HOY</title>
+</head>
+<body>
+ <link rel="stylesheet" type="text/css" href="estilo1.css">
+<div class="blockquote-wrapper">
+  <div class="blockquote">
+    
+    <h1>
+     La Fecha es: <span style="color:#FFDA33"><?php 
+echo date('d-m-y');
+
+?></span> Cada dia, es un dia menos para... <span style="color:#FFDA33"> Navidad.</span>
+     </h1>
+    <h4>&mdash;Paolo Alexander<br>
+Desarrolla Aplicaciones Web<br></h4>
+  </div>
+</div>
+</body>
+</html>
+
