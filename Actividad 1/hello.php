3f0fddb7ed00c9c60b9383ceb77721bbe89789c1
paoloshy <114453757+paoloshy@users.noreply.github.com>
2022-10-25 23:54:07 -0500

    Add files via upload

---
+++ Actividad 1/hello.php
@@ -0,0 +1,18 @@
+
+    <!DOCTYPE html>
+    <html>
+    <head>
+      <meta charset="utf-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+      <link rel="stylesheet" type="text/css" href="estilo.css">
+      <title>Hola</title>
+    </head>
+    <body>
+    <article>
+      <h1>Hola a todos</h1>
+      <p>Hello World Paolo Alexander</p>
+    </article>
+    </body>
+    </html>
+    
+    
